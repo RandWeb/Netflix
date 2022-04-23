72703b8a201383ac07263a7b3fba9b68763bbb9c
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-04-23 09:15:41 -0700

    complated row

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Row from './Row';
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 function App() {
   return (
     <div className="App">
       <h1>Hey Guys Programmer! Let's build Netflix Clone Front-End</h1>
-      <Row title="Netflix orginals" />
-      <Row title="Trending Now" />
+      <Row title="NetFlix Originals" fetchUrl={requests.fetchNetflixOriginal} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Actions Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-    const [movies, setMovies] = useState([]);
-    return (
-        <div>
-            <h2>{title}</h2>
-            {/** container */}
-        </div>
-    );
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+  return (
+    <div className="row">
+      <h2>{title} </h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+          key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
