4ce2ca15e0899b87eff554d77c7da82e6d501a38
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-04-26 14:43:55 -0700

    compalate clone

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
+import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer';
 import "./row.css";
+
+import MovieTrailer from "movie-trailer";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl ,isLargeRow = false}) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -14,6 +19,28 @@ function Row({ title, fetchUrl ,isLargeRow = false}) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "99%",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const handleClick = (movie) => {
+    // console.table(movie?.title)
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.name ||  "")
+        .then(url => {
+          console.log(url, movie)
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        }).catch((error) => console.log(error));
+    }
+  }
   return (
     <div className="row">
       <h2>{title} </h2>
@@ -21,11 +48,15 @@ function Row({ title, fetchUrl ,isLargeRow = false}) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${movie.poster_path}`}
-            alt={movie.name} 
+            alt={movie.name}
           />
         ))}
+      </div>
+      <div style={{ padding: "4rem" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
       </div>
     </div>
   );
---
+++ src/Youtube.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Youtube.css';
+
+
+function Youtube() {
+       
+}
+
+export default Youtube;
